694f719dfecc6f84dafa578e4f10cfdc66fc7b68
Thiago Morgado <Thiago Morgado>
2019-05-17 14:32:26 -0300

    atualizando
    atualizando
    
    Co-Authored-By: thiagomorgado <thiagomorgado@users.noreply.github.com>

--- classificados.sql
+++ classificados.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 11-Abr-2019 às 21:26
--- Versão do servidor: 5.7.24
--- versão do PHP: 7.2.14
+-- Generation Time: 17-Maio-2019 às 17:00
+-- Versão do servidor: 5.7.26
+-- versão do PHP: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,28 +30,28 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `anuncio`;
 CREATE TABLE IF NOT EXISTS `anuncio` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_usuario` int(11) NOT NULL,
   `id_categoria` int(11) NOT NULL,
   `titulo` varchar(100) DEFAULT NULL,
   `descricao` text,
   `valor` float DEFAULT NULL,
-  `estado` int(11) NOT NULL,
+  `estado` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categoria`
+-- Estrutura da tabela `categorias`
 --
 
-DROP TABLE IF EXISTS `categoria`;
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,20 +61,22 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(32) NOT NULL,
-  `telefone` varchar(30) DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `telefone`) VALUES
-(1, 'thiago do nascimento morgado', 'thiago-morgado@outlook.com.br', '202cb962ac59075b964b07152d234b70', '123');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `phone`) VALUES
+(1, 'thiago morgado', 'thiago_morgado@hotmail.com', '202cb962ac59075b964b07152d234b70', NULL),
+(2, 'fernando morgado', 'fernando_morgado@hotmail.com', '202cb962ac59075b964b07152d234b70', '11111111'),
+(3, 'thiago d nascimento morgado', 'thiago.morgado@hotmail.com', '202cb962ac59075b964b07152d234b70', '111110111');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
